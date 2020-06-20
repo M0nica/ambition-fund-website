2cca41d6d5f4176c860b2c6626c0ef3975bec30d
Monica Powell <monica@aboutmonica.com>
2020-06-20 18:08:46 -0400

    Initial commit from gatsby: (https://github.com/keegn/gatsby-starter-saas-marketing.git)

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Starter SaaS Marketing`,
+    description: `A simple one page marketing starter for saas companies.`,
+    author: `Keegan Burkett`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Poppins`,
+            variants: [`300`, `400`, `600`, `700`],
+          },
+        ],
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `product`,
+        path: `${__dirname}/src/images/product`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
