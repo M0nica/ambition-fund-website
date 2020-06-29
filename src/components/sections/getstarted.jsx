c0580d29811ff97cab19c0a21dae1b000b06c64a
Omar Atta <omaratta212@gmail.com>
2020-06-30 00:30:29 +0200

    Fix receive updates button style (#22)
    * Removed the button underline
    
    * Add button hover state
    
    * Add hover color variable to the theme

--- src/components/sections/getstarted.jsx
+++ src/components/sections/getstarted.jsx
@@ -7,10 +7,7 @@ const GetStarted = () => (
   <StyledSection>
     <GetStartedContainer>
       <GetStartedTitle>Keep In Touch</GetStartedTitle>
-      <a href="#top">
-        {" "}
-        <TryItButton>Receive Updates</TryItButton>
-      </a>
+      <TryItButton as="a" href="#top">Receive Updates</TryItButton>
       <Subtitle>
         Thanks for visiting!{" "}
         <span role="img" aria-label="hand waving">
@@ -47,7 +44,6 @@ const TryItButton = styled.button`
   color: white;
   letter-spacing: 1px;
   height: 60px;
-  display: block;
   margin-left: 8px;
   text-transform: uppercase;
   cursor: pointer;
@@ -60,7 +56,13 @@ const TryItButton = styled.button`
   border-color: initial;
   border-image: initial;
   outline: 0px;
+  display: inline-flex;
+  align-items: center;
+  text-decoration: none;
+  transition: all 0.2s ease;
   &:hover {
+    transform: translateY(-1px);
+    background: ${props => props.theme.color.hover};
     box-shadow: rgba(110, 120, 152, 0.22) 0px 2px 10px 0px;
   }
   @media (max-width: ${props => props.theme.screen.md}) {
